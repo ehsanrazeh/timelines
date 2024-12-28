2cc84f9c55c88ead07b667bee3ab82adb66b1a2e
ehsanrazeh <ehsan.razeh@gmail.com>
2024-12-28 23:47:45 +0000

    Update timelines.dart

--- lib/timelines.dart
+++ lib/timelines.dart
@@ -1,9 +1,9 @@
 /// Widgets that make it easy to implement the timeline UI component.
 library timelines;
 
-export 'package:timelines/src/connector_theme.dart';
+export 'package:timelines/src/connector_theme.dart' show ConnectorThemeData;
 export 'package:timelines/src/connectors.dart';
-export 'package:timelines/src/indicator_theme.dart';
+export 'package:timelines/src/indicator_theme.dart' hide ConnectorThemeData;
 export 'package:timelines/src/indicators.dart';
 export 'package:timelines/src/timelines.dart';
 export 'package:timelines/src/timeline_node.dart';
